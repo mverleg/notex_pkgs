aef13e70c6108dfd80513b05db55640059c1b46d
rafiki <mark@rafiki>
2016-02-29 23:56:23 +0100

    convert mathjax to using tags

--- mathjax/tags.py
+++ mathjax/tags.py
@@ -1,10 +1,17 @@
 
+from bs4 import NavigableString, BeautifulSoup
+from notexp.bases import TagHandler
 
-class InlineEquation:
-	""" add ['$[',']$'] """
+
+class InlineEquation(TagHandler):
+	def __call__(self, element, **kwargs):
+		BeautifulSoup.insert(element, 0, NavigableString('$['))
+		BeautifulSoup.append(element, NavigableString(']$'))
 
 
-class BlockEquation:
-	""" add ['$$[',']$$'] """
+class BlockEquation(TagHandler):
+	def __call__(self, element, **kwargs):
+		BeautifulSoup.insert(element, 0, NavigableString('$$['))
+		BeautifulSoup.append(element, NavigableString(']$$'))
 
 
